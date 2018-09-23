b1dc84fd50caa20833d591cef5888a007b882693
Srinivas Venbakam Vengadam <srinive@microsoft.com>
2018-09-23 22:18:20 +0530

    Logger first checkin

---
+++ LoggerDemo/Startup.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace LoggerDemo
+{
+    public class Startup
+    {
+        private readonly ILoggerFactory loggerFactory;
+        private readonly IConfigurationRoot configurationRoot;
+        private readonly IHostingEnvironment hostEnvironment;
+        
+        
+        public Startup(IHostingEnvironment hostingEnvironment, ILoggerFactory loggerFactory)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(hostingEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            configurationRoot = builder.Build();
+
+            this.loggerFactory = loggerFactory;
+            this.hostEnvironment = hostingEnvironment;
+        }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Step1
+
+            services.AddMvc();
+            services.AddApplicationInsightsTelemetry(configurationRoot.GetSection("ApplicationInsights:InstrumentationKey"));                 
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+            loggerFactory.AddDebug();
+            loggerFactory.AddProvider(new NLog.Extensions.Logging.NLogLoggerProvider());
+
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }           
+
+            //Step2
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=api}/{action=Home}/{id?}");
+            });
+            
+            
+        }
+    }
+}
---
+++ LoggerDemo/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LoggerDemo
+{
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        private ILogger<HomeController> logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            this.logger = logger;
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var controller = HttpContext.Request.Method.ToString();
+            logger.LogDebug($"In Get method srini {controller}");
+            logger.LogInformation($"In Get method srini {controller}");
+            return new string[] { "value1", "value2" };
+            
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
